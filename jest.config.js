60083a2ce283d01a6bc7b4040410a4bd8344a35f
Rupesh Ravindran <Rupesh.Ravindran@intlfcstone.com>
2021-09-14 15:29:34 +0100

    fix for test failing due to unknow file type .css

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   preset: "ts-jest",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  }
 };
